48bb7663f3ea0abd25e8e930ffc5d04bcf770cf3
kangyuri1114 <yuri3334@gmail.com>
2024-03-27 01:35:24 +0900

    [add]: MainActivity 파일 추가

--- app/src/main/java/com/zucchini/ssuplector/InitActivity.kt
+++ app/src/main/java/com/zucchini/ssuplector/InitActivity.kt
@@ -3,19 +3,20 @@ package com.zucchini.ssuplector
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.zucchini.ssuplector.databinding.ActivityMainBinding
+import com.zucchini.projects.MainActivity
+import com.zucchini.ssuplector.databinding.ActivityInitBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class InitActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInitBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityInitBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        // startActivity(Intent(this, LoginActivity::class.java))
-        // finish()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
---
+++ feature/projects/src/main/java/com/zucchini/projects/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.zucchini.projects
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zucchini.feature.projects.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
