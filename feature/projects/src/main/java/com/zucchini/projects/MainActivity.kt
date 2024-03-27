e9915104a8ce871554221997c7fdf24b375d5e2a
kangyuri1114 <yuri3334@gmail.com>
2024-03-27 20:18:22 +0900

    [feat]: top app bar, FAB 구현

--- feature/projects/src/main/java/com/zucchini/projects/MainActivity.kt
+++ feature/projects/src/main/java/com/zucchini/projects/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.zucchini.feature.projects.R
 import com.zucchini.feature.projects.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
