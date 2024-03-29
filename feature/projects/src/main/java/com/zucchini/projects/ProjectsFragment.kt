4c6804be827303ed30eee74455e5240e169cd137
kangyuri1114 <yuri3334@gmail.com>
2024-03-29 20:32:04 +0900

    [chore]: 파일 위치 수정

--- feature/projects/src/main/java/com/zucchini/projects/ProjectsFragment.kt
+++ feature/projects/src/main/java/com/zucchini/projects/ProjectsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.zucchini.feature.projects.databinding.FragmentProjectsBinding
+import com.zucchini.projects.adapter.SearchKeywordAdapter
 
 class ProjectsFragment : Fragment() {
     private var _binding: FragmentProjectsBinding? = null
