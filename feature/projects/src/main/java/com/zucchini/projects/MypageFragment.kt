a09887cbdecdc4d45c4964bcc9bccf1bc7870741
kangyuri1114 <yuri3334@gmail.com>
2024-04-21 03:04:01 +0900

    [chore]: 마이페이지 외부 웹 뷰 링크 이동 구현 -intent 구현

--- feature/projects/src/main/java/com/zucchini/projects/MypageFragment.kt
+++ feature/projects/src/main/java/com/zucchini/projects/MypageFragment.kt
@@ -1,19 +1,48 @@
 package com.zucchini.projects
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.zucchini.feature.projects.R
+import com.zucchini.feature.projects.databinding.FragmentMypageBinding
 
 class MypageFragment : Fragment() {
-
+    private var _binding: FragmentMypageBinding? = null
+    private val binding: FragmentMypageBinding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+    ): View {
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
+
+        navigateToWebDocs()
+        navigateToKakaoOpenChat()
+        return binding.root
+    }
+
+    private fun navigateToWebDocs() {
+        binding.tvTermsOfUse.setOnClickListener {
+            val privacyRuleUrl = getString(R.string.service_docs_uri)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(privacyRuleUrl))
+            startActivity(intent)
+        }
+        binding.tvPrivacyPolicy.setOnClickListener {
+            val privacyRuleUrl = getString(R.string.service_docs_uri)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(privacyRuleUrl))
+            startActivity(intent)
+        }
+    }
+
+    private fun navigateToKakaoOpenChat() {
+        binding.tvComplaint.setOnClickListener {
+            val kakaoOpenChatUrl = getString(R.string.tv_complaint_link)
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(kakaoOpenChatUrl))
+            startActivity(intent)
+        }
     }
 }
