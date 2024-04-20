dd322df75739a7064a47c58b5180d27b1d718be3
kangyuri1114 <yuri3334@gmail.com>
2024-04-21 05:44:43 +0900

    [feat]: 정렬 키워드 클릭 이벤트 구현

--- feature/projects/src/main/java/com/zucchini/projects/projects/ProjectsFragment.kt
+++ feature/projects/src/main/java/com/zucchini/projects/projects/ProjectsFragment.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.zucchini.domain.model.Keyword
 import com.zucchini.domain.model.KeywordList
 import com.zucchini.feature.projects.databinding.FragmentProjectsBinding
 import com.zucchini.projects.adapter.PageIndicatorAdapter
+import com.zucchini.projects.dummy.ProjectDummyList
 import com.zucchini.projects.projects.adapter.ProjectsAdapter
 import com.zucchini.projects.projects.adapter.SearchKeywordAdapter
-import com.zucchini.projects.dummy.ProjectDummyList
 
 class ProjectsFragment : Fragment() {
     private var _binding: FragmentProjectsBinding? = null
@@ -31,7 +32,7 @@ class ProjectsFragment : Fragment() {
         initKeywordAdapter()
         initProjectsAdapter()
         initPageIndicator()
-
+        setSortingKeyword()
         return binding.root
     }
 
@@ -49,11 +50,77 @@ class ProjectsFragment : Fragment() {
         binding.rvSearchKeyword.adapter = searchKeywordAdapter
         searchKeywordAdapter.submitList(KeywordList.searchKeyword.map { Keyword(it) })
     }
+
     private fun initPageIndicator() {
         pageIndicatorAdapter = PageIndicatorAdapter(requireContext(), totalPage)
         binding.rvPageIndicator.adapter = pageIndicatorAdapter
-        binding.rvPageIndicator.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvPageIndicator.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
     }
+
+    private fun setSortingKeyword() {
+        binding.tvSortRecent.setOnClickListener {
+            binding.tvSortRecent.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    com.zucchini.core.designsystem.R.color.olive_black,
+                ),
+            )
+            binding.tvSortHighCheck.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    com.zucchini.core.designsystem.R.color.gray1,
+                ),
+            )
+            binding.tvSortLowCheck.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    com.zucchini.core.designsystem.R.color.gray1,
+                ),
+            )
+        }
+        binding.tvSortHighCheck.setOnClickListener {
+            binding.tvSortRecent.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    com.zucchini.core.designsystem.R.color.gray1,
+                ),
+            )
+            binding.tvSortHighCheck.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    com.zucchini.core.designsystem.R.color.olive_black,
+                ),
+            )
+            binding.tvSortLowCheck.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    com.zucchini.core.designsystem.R.color.gray1,
+                ),
+            )
+        }
+        binding.tvSortLowCheck.setOnClickListener {
+            binding.tvSortRecent.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    com.zucchini.core.designsystem.R.color.gray1,
+                ),
+            )
+            binding.tvSortHighCheck.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    com.zucchini.core.designsystem.R.color.gray1,
+                ),
+            )
+            binding.tvSortLowCheck.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    com.zucchini.core.designsystem.R.color.olive_black,
+                ),
+            )
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
