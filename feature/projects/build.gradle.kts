235e1caeb26cc452c8d464c7b66ba1d5cdee4b23
kangyuri1114 <yuri3334@gmail.com>
2024-07-05 04:13:40 +0900

    [feat]: 이미지 업로드 구현

--- feature/projects/build.gradle.kts
+++ feature/projects/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(project(":core:designsystem"))
     implementation(project(":core:common"))
     implementation(project(":core:network"))
+    implementation(project(":data"))
 
     KotlinDependencies.run {
         implementation(kotlin)
--- feature/projects/src/main/java/com/zucchini/submit/SubmitProjectActivity.kt
+++ feature/projects/src/main/java/com/zucchini/submit/SubmitProjectActivity.kt
@@ -1,11 +1,31 @@
 package com.zucchini.submit
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import com.zucchini.data.ContentUriRequestBody
+import com.zucchini.feature.projects.R
 import com.zucchini.feature.projects.databinding.ActivitySubmitProjectBinding
+import com.zucchini.view.showShortToast
+import timber.log.Timber
 
 class SubmitProjectActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySubmitProjectBinding
+
+    private var imageUri = Uri.EMPTY
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            imageUri = uri
+            if (imageUri != null) {
+                binding.ivProjectSubmit.load(imageUri)
+                processSelectedImage(imageUri!!)
+            } else {
+                showShortToast(getString(R.string.submit_image))
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,11 +33,29 @@ class SubmitProjectActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         backClickListner()
+        initImageSubmitView()
     }
 
     private fun backClickListner() {
         binding.ivBackButton.setOnClickListener {
             finish()
         }
+    }
+
+    private fun initImageSubmitView() {
+        binding.tvProjectSubmitImage.setOnClickListener {
+            launcher.launch("image/*")
+        }
+    }
+
+    private fun processSelectedImage(uri: Uri) {
+        val requestBody = ContentUriRequestBody(
+            context = this,
+            uri = uri
+        ).toFormData()
+
+        Timber.d("Image Uri: $uri")
+        Timber.d("Image Path: ${uri.path}")
+        Timber.d("Request Body: $requestBody")
     }
 }
