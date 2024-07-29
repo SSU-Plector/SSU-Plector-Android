dd9cbeba3a73f9b77701ea27590834e15f4ecd44
kangyuri1114 <yuri3334@gmail.com>
2024-07-30 00:22:31 +0900

    [feat]: 개발자 매칭 UI 구현

--- app/src/main/java/com/zucchini/ssuplector/di/DefaultNavigationProvider.kt
+++ app/src/main/java/com/zucchini/ssuplector/di/DefaultNavigationProvider.kt
@@ -3,38 +3,39 @@ package com.zucchini.ssuplector.di
 import android.content.Context
 import android.content.Intent
 import com.zucchini.ai_members.designer.AiDesignerActivity
+import com.zucchini.ai_members.matching.AiMatchingBotActivity
 import com.zucchini.ai_members.pm.AiPmActivity
 import com.zucchini.auth.LoginActivity
 import com.zucchini.common.NavigationProvider
 import com.zucchini.projects.MainActivity
-import com.zucchini.submit.SubmitDevActivity
+import com.zucchini.submit.developer.SubmitDevActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class DefaultNavigationProvider
-    @Inject
-    constructor(
-        @ApplicationContext private val context: Context,
-    ) : NavigationProvider {
-        override fun toDevInfo(): Intent {
-            TODO("Not yet implemented")
-        }
+@Inject
+constructor(
+    @ApplicationContext private val context: Context,
+) : NavigationProvider {
+    override fun toDevInfo(): Intent {
+        TODO("Not yet implemented")
+    }
 
-        override fun toProjects(): Intent {
-            TODO("Not yet implemented")
-        }
+    override fun toProjects(): Intent {
+        TODO("Not yet implemented")
+    }
 
-        override fun toMyPage(): Intent {
-            TODO("Not yet implemented")
-        }
+    override fun toMyPage(): Intent {
+        TODO("Not yet implemented")
+    }
 
-        override fun toLogin(): Intent = Intent(context, LoginActivity::class.java)
+    override fun toLogin(): Intent = Intent(context, LoginActivity::class.java)
 
-        override fun toSubmitDev(): Intent = Intent(context, SubmitDevActivity::class.java)
+    override fun toSubmitDev(): Intent = Intent(context, SubmitDevActivity::class.java)
 
-        override fun toMain(): Intent = Intent(context, MainActivity::class.java)
+    override fun toMain(): Intent = Intent(context, MainActivity::class.java)
 
-        override fun toAiPmMember(): Intent = Intent(context, AiPmActivity::class.java)
+    override fun toAiPmMember(): Intent = Intent(context, AiPmActivity::class.java)
 
     override fun toAiDesignerMembers(): Intent = Intent(context, AiDesignerActivity::class.java)
 
